045e4cd13c9cf95d50e35de202ce64198c5f0da1
Vineet <vineetkushwaha6325@gmail.com>
2025-05-08 00:31:01 +0530

    done

--- Task3.py
+++ Task3.py
@@ -1,10 +1,22 @@
+import hashlib
+
+# def hash_password(password):
+#     pass = password.encode('utf-8') 
+#     hash_obj = haslib.sha256(pass)
+#     hash_pass = hash_obj.hexdigest() #hexadecimal
+#     return hash_pass
+
+def hash(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+# {'username': 'raml', 'password': '8a10191e65ccf722bf59e70ddda155525c5f2ab80c5fed2ed67ee9f855171f7d'}
+
 userList = [
     {"username": "John", "password": "123456"},
 ]
 def login(username,password):
     for user in userList:
         if user["username"] == username:
-            if user["password"] == password:
+            if hash(user["password"]) == hash(password):
                 print("Login successful")
                 return True
             else:
@@ -29,7 +41,7 @@ def register(username,password):
                 username = input("Username already exists. Please enter a new username: ")
                 if user["username"] != username:
                     break
-    userList.append({"username": username, "password": password})
+    userList.append({"username": username, "password": hash(password)})
     print("Registration successful")
 
 # Welcome to the login system
@@ -53,3 +65,19 @@ else:
     username = input("Please enter your username: ")
     password = input("Please enter your password: ")
     register(username,password)
+
+print("Display \n", userList)
+
+
+# Welcome to the login system
+# 1. Login
+# 2. Register
+# Please select an option: 2
+# Please enter your username: raml
+# Please enter your password: 3453
+# Registration successful
+# Display
+# [
+#     {'username': 'John', 'password': '123456'}, 
+#     {'username': 'raml', 'password': '8a10191e65ccf722bf59e70ddda155525c5f2ab80c5fed2ed67ee9f855171f7d'}
+# ]
